34f2d157015a47b532ecaf884c48c70c9d81237f
nDrew <89747751+nDrew1984@users.noreply.github.com>
2026-09-20 12:56:57 +0200

    updated api conftest

--- api-tests/conftest.py
+++ api-tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from api_endpoints import PRODUCTS_LIST, BRANDS_LIST
 
 @pytest.fixture(scope="session")
 def base_url():
@@ -10,3 +11,11 @@ def session(base_url):
     s = requests.Session()
     yield s
     s.close()
+
+@pytest.fixture
+def products_response(session, base_url):
+    return session.get(f"{base_url}{PRODUCTS_LIST}")
+
+@pytest.fixture
+def brands_response(session, base_url):
+    return session.get(f"{base_url}{BRANDS_LIST}")
