760652aac52f66ec67754b4c0d16cbeef8fde44e
nDrew <89747751+nDrew1984@users.noreply.github.com>
2026-09-20 11:42:28 +0200

    updated api-schemas: added  brands list

--- api-tests/api_schemas.py
+++ api-tests/api_schemas.py
@@ -12,3 +12,18 @@ PRODUCTS_LIST_SCHEMA = {
     },
     "required": ["responseCode", "products"]
 }
+
+BRANDS_LIST_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "responseCode": {"type": "number"},
+        "brands": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "required": ["id", "brand"]
+            }
+        }
+    },
+    "required": ["responseCode", "brands"]
+};
